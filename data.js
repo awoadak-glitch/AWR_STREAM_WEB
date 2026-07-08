1b289559533d76fd56f0c2b3dc5e1b6c332a7100
awoadak-glitch <awoadak@gmail.com>
2026-07-08 22:21:09 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,9 @@
+const mockData = [
+    { id: 1, title: 'Inception', type: 'فيلم', year: '2010', rating: '8.8', image: 'https://image.tmdb.org/t/p/w200/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg' },
+    { id: 2, title: 'Attack on Titan', type: 'أنمي', year: '2013', rating: '9.0', image: 'https://image.tmdb.org/t/p/w200/hTP1DtLGFamjfu8WqjnuQdP1n4i.jpg' },
+    { id: 3, title: 'Squid Game', type: 'مسلسل', year: '2021', rating: '8.0', image: 'https://image.tmdb.org/t/p/w200/dDlEmu3EZ0Pgg93K2SVNLCjCSvE.jpg' },
+    { id: 4, title: 'Vincenzo', type: 'كيدراما', year: '2021', rating: '8.5', image: 'https://image.tmdb.org/t/p/w200/dvXJgEDQXhL9Ouot2WkBHpQiHGd.jpg' },
+    { id: 5, title: 'Interstellar', type: 'فيلم', year: '2014', rating: '8.6', image: 'https://image.tmdb.org/t/p/w200/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg' },
+    { id: 6, title: 'Breaking Bad', type: 'مسلسل', year: '2008', rating: '9.5', image: 'https://image.tmdb.org/t/p/w200/ggFHVNu6YYI5L9pCfOacjizRFlj.jpg' },
+    { id: 7, title: 'Death Note', type: 'أنمي', year: '2006', rating: '9.0', image: 'https://image.tmdb.org/t/p/w200/tC9zQzHjF7O0UqL4e9A8H8w4G8q.jpg' }
+];
