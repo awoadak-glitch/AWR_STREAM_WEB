02273f89bc5cebd8d3f27c21764bf2685e8997b9
awoadak-glitch <awoadak@gmail.com>
2026-09-08 08:17:21 -0700

    Preview native Android UI rebuild

--- bootstrap.cjs
+++ bootstrap.cjs
@@ -3,7 +3,7 @@ const path = require('path');
 const https = require('https');
 const { execFileSync } = require('child_process');
 
-const COMMIT = '031222baaed972b2554868a2dd263b2a270a9faf';
+const COMMIT = '1010575acab824c4fd947c68a2e92051a7c6485a';
 const ARCHIVE = `/tmp/anime-witcher-${COMMIT}.tar.gz`;
 const URL = `https://codeload.github.com/awoadak-glitch/STREMING_WEP/tar.gz/${COMMIT}`;
 
