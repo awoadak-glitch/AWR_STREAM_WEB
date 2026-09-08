4014d326287a00f6ea30105f14cc144769d9c20a
awoadak-glitch <awoadak@gmail.com>
2026-09-08 10:34:53 -0700

    Deploy working Anime Witcher category lists

--- bootstrap.cjs
+++ bootstrap.cjs
@@ -3,7 +3,7 @@ const path = require('path');
 const https = require('https');
 const { execFileSync } = require('child_process');
 
-const COMMIT = 'da08e9252b70542cb6fac6d6ba65e7b8f25ac2f1';
+const COMMIT = '1d253322e618ce821f630a259ee3bc481e730644';
 const ARCHIVE = `/tmp/anime-witcher-${COMMIT}.tar.gz`;
 const URL = `https://codeload.github.com/awoadak-glitch/STREMING_WEP/tar.gz/${COMMIT}`;
 
